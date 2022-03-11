01418e0ee5a36c6c4a4979b329de62201984087b
xh <xianghao98@163.com>
2022-03-11 15:35:33 +0800

    根据父权限id查询子权限Mapper

--- src/main/java/org/xianghao/eshop/auth/mapper/PriorityMapper.java
+++ src/main/java/org/xianghao/eshop/auth/mapper/PriorityMapper.java
@@ -1,9 +1,6 @@
 package org.xianghao.eshop.auth.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.xianghao.eshop.auth.domain.PriorityDO;
 
 import java.util.List;
@@ -36,4 +33,27 @@ public interface PriorityMapper {
             @Result(column = "gmt_modified", property = "gmtModified")
     })
     List<PriorityDO> listRootPriorities();
+
+
+    /**
+     * 根据父权限id查询子权限
+     * @param parentId
+     * @return 子权限
+     * */
+    @Select("SELECT " +
+            "id,code,url,priority_comment,priority_type,parent_id,gmt_create,gmt_modified " +
+            "FROM auth_priority" +
+            "WHERE parent_id = #{parentId}")
+    @Results({
+            @Result(column = "id",property = "id",id = true),
+            @Result(column = "code", property = "code"),
+            @Result(column = "url", property = "url"),
+            @Result(column = "priority_comment", property = "priorityComment"),
+            @Result(column = "priority_type", property = "priorityType"),
+            @Result(column = "parent_id", property = "parentId"),
+            @Result(column = "gmt_create", property = "gmtCreate"),
+            @Result(column = "gmt_modified", property = "gmtModified")
+    })
+    List<PriorityDO> listChildPriorities(@Param("parentId") Long parentId);
+
 }
